cbc62a281adf48d0c1e2bb8a3ac47eb95716d80d
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2022-10-15 10:44:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import ViewItems from "./components/ViewItems";
+export default function App() {
+  return (
+    <div className="App">
+      <ViewItems />
+    </div>
+  );
+}
